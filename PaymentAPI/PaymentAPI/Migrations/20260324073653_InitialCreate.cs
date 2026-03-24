17e8f4542a34a3c0efd823bd858669fdb2b04564
Osagani31 <naonperera@students.nsbm.ac.lk>
2026-03-24 21:08:56 +0530

    implement API
    implemented payment API

---
+++ PaymentAPI/PaymentAPI/Migrations/20260324073653_InitialCreate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaymentAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PaymentDetailsId",
+                table: "PaymentDetails",
+                newName: "PaymentDetailId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PaymentDetailId",
+                table: "PaymentDetails",
+                newName: "PaymentDetailsId");
+        }
+    }
+}
